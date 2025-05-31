a79ea57b049e6153c20c6eb777bb152ae6d08368
Ammar Shahab <ammar.qa@generalpharma.com>
2025-05-31 15:11:26 +0600

    updated

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -2,24 +2,17 @@ import React, { Suspense } from "react";
 import ApplicationList from "./ApplicationList";
 import useAuth from "../../hooks/useAuth";
 
-
-
-//  Note: If u need the data of user.email in other section of website u can export it from here or u can make a folder name api under src folder and create another file applicationsPromise.js then export it from there. this is for large application.
+// 4.12 as we are using fetch we use {credentials: "include",} after the url. Note: as the method is get so by default it will be a get method but in put or patch then method should be "method: PUT" included for the fetch method. Now reload the my Apllication in browser and see the server u will get the token.
 const applicationsPromise = (email) => {
-  return fetch(`http://localhost:3000/applications?email=${email}`).then(
-    (res) => res.json()
-  );
+  return fetch(`http://localhost:3000/applications?email=${email}`, {
+    credentials: "include",
+  }).then((res) => res.json());
 };
 
-
 const MyApplications = () => {
- 
-
- 
   const { user } = useAuth();
   return (
     <Suspense fallback={"Your Application is loading . . ."}>
-     
       <ApplicationList
         applicationsPromise={applicationsPromise(user.email)}
       ></ApplicationList>
